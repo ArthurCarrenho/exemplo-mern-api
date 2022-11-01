db62a0c39d010c0e2cd87bd411f0563317097645
Arthur Carrenho <arthur.carrenho@unifacef.com.br>
2022-11-01 20:01:12 -0300

    erro meu

--- server.js
+++ server.js
@@ -66,9 +66,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-    res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     console.log(`Server listening on the port  ${port}`);
 })
