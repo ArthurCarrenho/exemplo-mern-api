3f395a0c573de527ddf2bd80325e3bea7cc66006
Arthur Carrenho <arthur.carrenho@unifacef.com.br>
2022-11-01 19:59:27 -0300

    cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 console.log('environment    ', process.env.ENVIRONMENT)
 console.log('PORT    ', process.env.PORT)
@@ -19,30 +20,55 @@ const port = process.env.PORT || 3080;
 
 app.use(express.static(path.join(__dirname, './ui/build')));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/api/tasks', (req, res) => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     taskController.getTasks().then(data => res.json(data));
 });
 
 app.post('/api/task', (req, res) => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     console.log(req.body);
     taskController.createTask(req.body.task).then(data => res.json(data));
 });
 
 app.put('/api/task', (req, res) => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     taskController.updateTask(req.body.task).then(data => res.json(data));
 });
 
 app.delete('/api/task/:id', (req, res) => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     taskController.deleteTask(req.params.id).then(data => res.json(data));
 });
 
 app.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.sendFile(path.join(__dirname, './ui/build/index.html'));
 });
 
 
 
 app.listen(port, () => {
+    res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     console.log(`Server listening on the port  ${port}`);
 })
